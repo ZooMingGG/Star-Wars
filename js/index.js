91a8d8ccb2090475fea1cc37ea2cf6d199912134
ZooMingGG <volodia.lukashewwich@gmail.com>
2020-06-07 12:22:26 +0300

    did slider, add animate.css

---
+++ js/index.js
@@ -0,0 +1,23 @@
+"use strict";
+$( document ).ready(function() {
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        navClass:['left-arrow', 'right-arrow'],
+        margin:20,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            900:{
+                items:3
+            },
+            1200:{
+                items:4
+            }
+        }
+    });
+});
