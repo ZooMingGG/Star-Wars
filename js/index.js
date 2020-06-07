19cc0351ecd76504477adefb17de392744e03257
ZooMingGG <volodia.lukashewwich@gmail.com>
2020-06-07 15:36:59 +0300

    did second bg, animation for text and btn

--- js/index.js
+++ js/index.js
@@ -1,23 +1,14 @@
 "use strict";
-$( document ).ready(function() {
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        navClass:['left-arrow', 'right-arrow'],
-        margin:20,
-        nav:true,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:2
-            },
-            900:{
-                items:3
-            },
-            1200:{
-                items:4
-            }
-        }
-    });
-});
+const body = document.querySelector('body');
+
+let current = 1;
+
+const timerId = setInterval( () => {
+    if (current % 2 === 0) {
+        body.classList.add('second-background');
+    } else {
+        body.classList.remove('second-background');
+    }
+
+    current++;
+}, 15000);
