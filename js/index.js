c73f474755c862d871b39c12625e8dede77613b6
ZooMingGG <volodia.lukashewwich@gmail.com>
2020-06-12 18:22:17 +0300

    add 2 more background variables and did some media

--- js/index.js
+++ js/index.js
@@ -3,12 +3,18 @@ const body = document.querySelector('body');
 
 let current = 1;
 
-const timerId = setInterval( () => {
+function changeBackground() {
     if (current % 2 === 0) {
+        body.classList.remove('first-background');
         body.classList.add('second-background');
     } else {
         body.classList.remove('second-background');
+        body.classList.add('first-background');
     }
 
     current++;
-}, 15000);
+}
+
+changeBackground();
+
+const timerId = setInterval(changeBackground, 15000);
